cb717d0b4b7f393e54e6c4b84d94700ba5f4c912
ggmood <misterartm612@gmail.com>
2025-03-31 19:52:46 +0300

    Update скрипт бд.sql

--- скрипт бд.sql
+++ скрипт бд.sql
@@ -1,37 +1,34 @@
--- Создание базы данных
-CREATE DATABASE UserAuthDB;
+CREATE DATABASE [UserAuthDB]
 GO
 
--- Использование созданной базы данных
-USE UserAuthDB;
+USE [UserAuthDB]
 GO
 
--- Создание таблицы User
-CREATE TABLE [User] (
-    ID INT IDENTITY(1,1) PRIMARY KEY,
-    Login NVARCHAR(50) NOT NULL,
-    Password NVARCHAR(50) NOT NULL,
-    Role NVARCHAR(50) NOT NULL,
-    FIO NVARCHAR(100) NOT NULL,
-    Gender NVARCHAR(10),
-    Phone NVARCHAR(20),
-    Photo NVARCHAR(255)
-);
+CREATE TABLE [dbo].[User](
+    [ID] [int] IDENTITY(1,1) NOT NULL,
+    [Logins] [nvarchar](50) NOT NULL,
+    [Passwords] [nvarchar](50) NOT NULL,
+    [Roles] [nvarchar](50) NOT NULL,
+    [FIO] [nvarchar](100) NOT NULL,
+    [Gender] [nvarchar](10) NULL,
+    [PhoneNumber] [nvarchar](20) NULL,
+    [PhotoURL] [nvarchar](255) NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+)
 GO
 
--- Заполнение таблицы тестовыми данными
-INSERT INTO [User] (Login, Password, Role, FIO, Gender, Phone, Photo)
+INSERT INTO [dbo].[User] ([Logins], [Passwords], [Roles], [FIO], [Gender], [PhoneNumber], [PhotoURL]) 
 VALUES 
-('admin', 'admin123', 'Администратор', 'Иванов Иван Иванович', 'Мужской', '+7(123)456-78-90', 'https://example.com/admin.jpg'),
-('manager1', 'mng123', 'Менеджер', 'Петрова Анна Сергеевна', 'Женский', '+7(234)567-89-01', 'https://example.com/manager1.jpg'),
-('manager2', 'mng456', 'Менеджер', 'Сидоров Дмитрий Петрович', 'Мужской', '+7(345)678-90-12', 'https://example.com/manager2.jpg'),
-('user1', 'user123', 'Пользователь', 'Кузнецова Елена Владимировна', 'Женский', '+7(456)789-01-23', 'https://example.com/user1.jpg'),
-('user2', 'user456', 'Пользователь', 'Федоров Михаил Александрович', 'Мужской', '+7(567)890-12-34', 'https://example.com/user2.jpg'),
-('user3', 'user789', 'Пользователь', 'Смирнова Ольга Игоревна', 'Женский', '+7(678)901-23-45', 'https://example.com/user3.jpg'),
-('guest1', 'guest123', 'Гость', 'Николаев Артем Сергеевич', 'Мужской', '+7(789)012-34-56', 'https://example.com/guest1.jpg'),
-('guest2', 'guest456', 'Гость', 'Васильева Татьяна Дмитриевна', 'Женский', '+7(890)123-45-67', 'https://example.com/guest2.jpg'),
-('test1', 'test123', 'Тестировщик', 'Григорьев Павел Андреевич', 'Мужской', '+7(901)234-56-78', 'https://example.com/test1.jpg'),
-('test2', 'test456', 'Тестировщик', 'Белова Юлия Олеговна', 'Женский', '+7(012)345-67-89', 'https://example.com/test2.jpg'),
-('dev1', 'dev123', 'Разработчик', 'Козлов Алексей Викторович', 'Мужской', '+7(123)456-78-91', 'https://example.com/dev1.jpg'),
-('dev2', 'dev456', 'Разработчик', 'Орлова Марина Евгеньевна', 'Женский', '+7(234)567-89-12', 'https://example.com/dev2.jpg');
+    ('admin', 'admin123', 'РђРґРјРёРЅРёСЃС‚СЂР°С‚РѕСЂ', 'РРІР°РЅРѕРІ РРІР°РЅ РРІР°РЅРѕРІРёС‡', 'РњСѓР¶СЃРєРѕР№', '+7(123)456-78-90', 'https://example.com/admin.jpg'),
+    ('manager1', 'mng123', 'РњРµРЅРµРґР¶РµСЂ', 'РџРµС‚СЂРѕРІР° РђРЅРЅР° РЎРµСЂРіРµРµРІРЅР°', 'Р–РµРЅСЃРєРёР№', '+7(234)567-89-01', 'https://example.com/manager1.jpg'),
+    ('manager2', 'mng456', 'РњРµРЅРµРґР¶РµСЂ', 'РЎРёРґРѕСЂРѕРІ Р”РјРёС‚СЂРёР№ РџРµС‚СЂРѕРІРёС‡', 'РњСѓР¶СЃРєРѕР№', '+7(345)678-90-12', 'https://example.com/manager2.jpg'),
+    ('user1', 'user123', 'РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ', 'РљСѓР·РЅРµС†РѕРІР° Р•Р»РµРЅР° Р’Р»Р°РґРёРјРёСЂРѕРІРЅР°', 'Р–РµРЅСЃРєРёР№', '+7(456)789-01-23', 'https://example.com/user1.jpg'),
+    ('user2', 'user456', 'РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ', 'Р¤РµРґРѕСЂРѕРІ РњРёС…Р°РёР» РђР»РµРєСЃР°РЅРґСЂРѕРІРёС‡', 'РњСѓР¶СЃРєРѕР№', '+7(567)890-12-34', 'https://example.com/user2.jpg'),
+    ('user3', 'user789', 'РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ', 'РЎРјРёСЂРЅРѕРІР° РћР»СЊРіР° РРіРѕСЂРµРІРЅР°', 'Р–РµРЅСЃРєРёР№', '+7(678)901-23-45', 'https://example.com/user3.jpg'),
+    ('guest1', 'guest123', 'Р“РѕСЃС‚СЊ', 'РќРёРєРѕР»Р°РµРІ РђСЂС‚РµРј РЎРµСЂРіРµРµРІРёС‡', 'РњСѓР¶СЃРєРѕР№', '+7(789)012-34-56', 'https://example.com/guest1.jpg'),
+    ('guest2', 'guest456', 'Р“РѕСЃС‚СЊ', 'Р’Р°СЃРёР»СЊРµРІР° РўР°С‚СЊСЏРЅР° Р”РјРёС‚СЂРёРµРІРЅР°', 'Р–РµРЅСЃРєРёР№', '+7(890)123-45-67', 'https://example.com/guest2.jpg'),
+    ('test1', 'test123', 'РўРµСЃС‚РёСЂРѕРІС‰РёРє', 'Р“СЂРёРіРѕСЂСЊРµРІ РџР°РІРµР» РђРЅРґСЂРµРµРІРёС‡', 'РњСѓР¶СЃРєРѕР№', '+7(901)234-56-78', 'https://example.com/test1.jpg'),
+    ('test2', 'test456', 'РўРµСЃС‚РёСЂРѕРІС‰РёРє', 'Р‘РµР»РѕРІР° Р®Р»РёСЏ РћР»РµРіРѕРІРЅР°', 'Р–РµРЅСЃРєРёР№', '+7(012)345-67-89', 'https://example.com/test2.jpg'),
+    ('dev1', 'dev123', 'Р Р°Р·СЂР°Р±РѕС‚С‡РёРє', 'РљРѕР·Р»РѕРІ РђР»РµРєСЃРµР№ Р’РёРєС‚РѕСЂРѕРІРёС‡', 'РњСѓР¶СЃРєРѕР№', '+7(123)456-78-91', 'https://example.com/dev1.jpg'),
+    ('dev2', 'dev456', 'Р Р°Р·СЂР°Р±РѕС‚С‡РёРє', 'РћСЂР»РѕРІР° РњР°СЂРёРЅР° Р•РІРіРµРЅСЊРµРІРЅР°', 'Р–РµРЅСЃРєРёР№', '+7(234)567-89-12', 'https://example.com/dev2.jpg')
 GO
